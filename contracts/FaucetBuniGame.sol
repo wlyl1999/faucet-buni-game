36a1283f0313ccdcfca210db11686632e5c28b14
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-02-07 16:53:58 +0700

    Add method to  mintTrainer

--- contracts/FaucetBuniGame.sol
+++ contracts/FaucetBuniGame.sol
@@ -47,11 +47,24 @@ contract  FaucetBuniGame is Initializable, AccessControlUpgradeable {
     function mintBunicorn(uint8 quantity) public {
         require(quantity <= maxMintedBunicorn, "Too much bunicorns");
         require(numberOfMintedBunicorn[msg.sender] <= maxMintedBunicorn - quantity, "Too much bunicorns");
-        for (uint8 i = numberOfMintedBunicorn[msg.sender]; i < quantity; i++) {
-            uint256 seed = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, i)));
+        for (uint8 i = 0; i < quantity; i++) {
+            uint256 seed = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, i + numberOfMintedBunicorn[msg.sender])));
             uint8 star = uint8(seed % 5);
             bunicornRoller.mintOneRandomBunicornWithStar(msg.sender, star, seed);
         }
         numberOfMintedBunicorn[msg.sender] += quantity;
+    }
+
+    function mintTrainer(uint8 quantity) public {
+        require(quantity <= maxMintedTrainer, "Too much trainers");
+        require(numberOfMintedTrainer[msg.sender] <= maxMintedTrainer - quantity, "Too much trainers");
+        for (uint8 i = 0; i < quantity; i++) {
+            uint256 seed = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, i + numberOfMintedTrainer[msg.sender])));
+            uint8 element = uint8(seed % 4);
+            uint8 level = uint8(uint256(keccak256(abi.encodePacked(gasleft(), seed))) % 20);
+            ITrainersV2 trainersContract = trainerRoller.getTrainersContract();
+            trainersContract.mintOneTrainerBySpecsByAdmin(msg.sender, element, level);
+        }
+        numberOfMintedTrainer[msg.sender] += quantity;
     }
 }
